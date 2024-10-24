5d0d048f9b99af2077dd2cc117f8ee2bfabd9d78
Chris Becker <christopher.becker@ro.co>
2024-10-23 21:38:35 -0400

    xp.css

---
+++ server.js
@@ -0,0 +1,10 @@
+var liveServer = require("live-server");
+
+var params = {
+    port: 8080,
+    wait: 1000,
+    open: false, // don't open browser
+    logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+};
+
+liveServer.start(params);
